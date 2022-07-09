4dec1b12b22d058bc32fae41d3124bdd8298faaa
Armin <92685045+ArminRmt@users.noreply.github.com>
2022-07-09 17:01:41 +0430

    Update test.py

--- Pete the baker/test.py
+++ Pete the baker/test.py
@@ -1,1 +1,14 @@
+# solutuon 1
 
+def cakes(recipe, available):
+    try:
+        return min([available[a]/recipe[a] for a in recipe])
+    except:
+        return 0
+
+
+# solutuon 2
+
+def cakes(recipe, available):
+
+    return min([available[i]//recipe[i] if i in available else 0 for i in recipe])
